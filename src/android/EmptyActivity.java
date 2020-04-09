edf716a62f2adbade62c2b4c34d0eaaf15150d6b
lmauer <luka.mauer@intendanet.hr>
2020-04-09 11:58:26 +0200

    in

--- src/android/EmptyActivity.java
+++ src/android/EmptyActivity.java
@@ -1,6 +1,19 @@
 package hr.intendanet.cordova.plugin.blank;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import org.apache.cordova.Config;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.util.Log;
 
 public class EmptyActivity extends Activity {
 
